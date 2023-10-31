f4d593ecc66ad287b4f96f4b71ea1f317bf2ef0f
Lloyd Woolacott <123055990+lwooly@users.noreply.github.com>
2023-10-31 18:40:15 +0000

    change for loop to map

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,27 @@
 import { Box, Grid } from '@mui/material';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import RegionalMap from '../components/RegionalMap';
 import ForecastWidget from '../components/ForecastWidget';
 import ForecastCarousel from '../components/ForecastCarousel';
 import HomeTitle from '../components/HomeTitle';
-import { useSelector } from 'react-redux';
 import { selectAllStyles } from '../features/slices/stylesSlice';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { fetchRegionalData, fetchAreaFromPostCode, fetchUserLocationAndPostcode } from '../features/slices/regionalForecastSlice';
+import {
+  fetchRegionalData,
+  fetchAreaFromPostCode,
+  fetchUserLocationAndPostcode,
+} from '../features/slices/regionalForecastSlice';
 
-const Home = () => {
+function Home() {
   const { padding, margin } = useSelector(selectAllStyles);
 
   const dispatch = useDispatch();
-  //loaded regional data state from redux store for graphics on this page
+  // loaded regional data state from redux store for graphics on this page
 
-
-  //get user location and postcode
-  const userLocationStatus = useSelector(state => state.regionalForecast.userLocation.status);
+  // get user location and postcode
+  const userLocationStatus = useSelector(
+    (state) => state.regionalForecast.userLocation.status
+  );
 
   useEffect(() => {
     if (userLocationStatus === 'idle') {
@@ -25,9 +29,13 @@ const Home = () => {
     }
   }, [userLocationStatus, dispatch]);
 
-  //get search area from postcode
-  const postcode = useSelector(state => state.regionalForecast.userLocation.postcode);
-  const areaSearchStatus = useSelector(state => state.regionalForecast.searchArea.status);
+  // get search area from postcode
+  const postcode = useSelector(
+    (state) => state.regionalForecast.userLocation.postcode
+  );
+  const areaSearchStatus = useSelector(
+    (state) => state.regionalForecast.searchArea.status
+  );
 
   useEffect(() => {
     if (userLocationStatus === 'loaded') {
@@ -35,8 +43,9 @@ const Home = () => {
     }
   }, [dispatch, postcode]);
 
-
-  const regionalDataState = useSelector(state => state.regionalForecast.status);
+  const regionalDataState = useSelector(
+    (state) => state.regionalForecast.status
+  );
 
   useEffect(() => {
     if (regionalDataState === 'idle') {
@@ -44,35 +53,25 @@ const Home = () => {
     }
   }, [regionalDataState, dispatch]);
 
-    
-
-    
-   
-
-
-    
   console.log(userLocationStatus);
 
-  
-    
-
-
   return (
-        <>
-            <Grid container spacing={2} sx={{ p: 1, maxWidth: '100%', overflow: 'hidden' }}>
-                <Grid item md={8}>
-                    <Box sx={{ mb: { xs: 1, md: 0 } }}>
-                        <HomeTitle />
-                    </Box>
-                    <ForecastWidget />
-                </Grid>
-                <Grid item md={4}>
-                    <RegionalMap />
-                </Grid>
-            </Grid>
-        </>
-
+    <Grid
+      container
+      spacing={2}
+      sx={{ p: 1, maxWidth: '100%', overflow: 'hidden' }}
+    >
+      <Grid item md={8}>
+        <Box sx={{ mb: { xs: 1, md: 0 } }}>
+          <HomeTitle />
+        </Box>
+        <ForecastWidget />
+      </Grid>
+      <Grid item md={4}>
+        <RegionalMap />
+      </Grid>
+    </Grid>
   );
-};
+}
 
 export default Home;
