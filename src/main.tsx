c2dfd2d19fcc85df0864f0b9b8d8c8da127c2f34
Lloyd Woolacott <123055990+lwooly@users.noreply.github.com>
2024-02-27 16:19:56 +1000

    titles and favicon updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import {BrowserRouter as Router }  from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+// eslint-disable-next-line import/extensions
 import App from './App.tsx';
 import store from './app/store';
 
