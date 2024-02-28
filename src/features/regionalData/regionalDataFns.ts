08d4705c349141111f589090c07472a56abb194e
Lloyd Woolacott <123055990+lwooly@users.noreply.github.com>
2024-02-28 14:12:03 +1000

    updated yellow to improve white text contrast

--- src/features/regionalData/regionalDataFns.ts
+++ src/features/regionalData/regionalDataFns.ts
@@ -7,7 +7,7 @@ export type IntensityColors = {
 export const intensityColors: IntensityColors = {
   'very high': 'hsla(0, 77%, 39%, 1.0)',
   high: 'hsla(24, 100%, 47%, 1.0)',
-  moderate: 'hsla(54, 100%, 71%, 1.0)',
+  moderate: 'hsla(54, 100%, 45%, 1.0)',
   low: 'hsla(123, 42%, 63%, 1.0)',
   'very low': 'hsla(162, 60%, 26%, 1.0)',
 };
